832fe0aa4e31681274cfe4172a309b25b1740856
ShunyaYamagami <4623533@ed.tus.ac.jp>
2023-08-12 16:54:44 +0900

    cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,44 @@ function process_args {
     local dset_num=$3
 
     # 残りの名前付き引数を解析
-    local dataset="Office31"
+    local dataset="OfficeHome"
+
+
+    local params=$(getopt -n "$0" -o p:c: -l dataset:,prod:,dev:,task_temp:,resume:,ssh_num:,tmux: -- "$@")
+    eval set -- "$params"
+
+    while true; do
+        case "$1" in
+            -p|--dataset)
+                dataset="$2"
+                shift 2
+                ;;
+            --task_temp)
+                task_temp="$2"
+                shift 2
+                ;;
+            --tmux)
+                tmux_session="$2"
+                shift 2
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "不明な引数: $1" >&2
+                return 1
+                ;;
+        esac
+    done
+    echo "gpu_i: $gpu_i"
+    echo "exec_num: $exec_num"
+    echo "dset_num: $dset_num"
+    echo "dataset: $dataset"
+    echo "tmux: $tmux_session"
+    echo -e ''  # (今は使っていないが)改行文字は echo コマンドに -e オプションを付けて実行した場合にのみ機能する.
+    
+    
     if [ $dataset = 'Office31' ]; then
         local task=(
             # "original_uda"
@@ -18,9 +55,10 @@ function process_args {
     elif [ $dataset = 'OfficeHome' ]; then
         local task=(
             # "original_uda"
-            "true_domains"
-            "simclr_rpl_dim128_wght0.5_bs512_ep3000_g3_encoder_outdim64_shfl"
-            "simclr_bs512_ep1000_g3_shfl"
+            # "true_domains"
+            # "simclr_rpl_dim128_wght0.5_bs512_ep3000_g3_encoder_outdim64_shfl"
+            # "simclr_bs512_ep1000_g3_shfl"
+            "contrastive_rpl_dim128_wght0.6_AE_bs512_ep3000_outd64_g3"
         )
     fi
 
@@ -63,6 +101,28 @@ function process_args {
                     --img_size 256
                 "
             done
+        elif [[ $dset_num == *"_"* ]]; then  # アンダーラインが含まれているかチェック
+            # アンダーラインで文字列を分割
+            IFS='_' read -r -a dset_num_list <<< "$dset_num"
+            # 配列の各要素をfor文でループ
+            for num in "${dset_num_list[@]}"; do
+                dset=${dsetlist[$num]}
+                COMMAND+=" && CUDA_VISIBLE_DEVICES=$gpu_i  python3 main.py \
+                    --train_batch_size 20 \
+                    --dataset $dataset \
+                    --dset $dset \
+                    --task $tsk \
+                    --model_type ViT-B_16 \
+                    --pretrained_dir checkpoint/imagenet21k_ViT-B_16.npz \
+                    --num_steps 5000 \
+                    --beta 0.1 \
+                    --gamma 0.01 \
+                    --use_im \
+                    --theta 0.1 \
+                    --img_size 256
+                "
+            done
+        
         else
             dset=${dsetlist[$dset_num]}
             COMMAND+=" && CUDA_VISIBLE_DEVICES=$gpu_i  python3 main.py \
@@ -85,7 +145,15 @@ function process_args {
     ###### 実行. 
     echo $COMMAND
     echo ''
-    eval $COMMAND
+
+    if [ -n "$tmux_session" ]; then
+        # 第3引数が存在する場合の処理. tmux内で実行する. $tmux_sessionはtmuxのセッション名.
+        tmux -2 new -d -s $tmux_session
+        tmux send-key -t $tmux_session.0 "$COMMAND" ENTER
+    else
+        # 第3引数が存在しない場合の処理. そのまま実行.
+        eval $COMMAND
+    fi
 }
 
 # 最初の3つの引数をチェック
---
+++ do_memo.sh
@@ -0,0 +1,3 @@
+. cmd.sh  1 0 1  --task simclr_bs512_ep1000_g3_shfl  --dataset OfficeHome  --tmux TVT_autoencoder_0_1_2
+. cmd.sh  0 0 0_1_2  --task contrastive_rpl_dim128_wght0.6_AE_bs512_ep3000_outd64_g3  --dataset OfficeHome  --tmux SDAT_2
+. cmd.sh  1 0 3_4_5  --task contrastive_rpl_dim128_wght0.6_AE_bs512_ep3000_outd64_g3  --dataset OfficeHome  --tmux TVT_autoencoder_345
